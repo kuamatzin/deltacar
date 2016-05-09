909bc55f4b72a3f26ca83784e70234ec86dcd4d5
Carlos Cuamatzin <kuamatzin@gmail.com>
2016-05-09 17:23:30 -0500

    Test arhivo de reporte 6

--- resources/views/ordenes/reporte.blade.php
+++ resources/views/ordenes/reporte.blade.php
@@ -1,9 +1,8 @@
 @extends('reporte')
 
 @section('content')
-http://admin.deltacar.mx/reportes/{{$orden->archivo_cotizacion}}
 <iframe 
-    src="http://docs.google.com/viewer?url=<?=urlencode('http://admin.deltacar.mx/reportes/{{$orden->archivo_cotizacion}}')?>&embedded=true" 
+    src="http://docs.google.com/gview?url=http://admin.deltacar.mx/reportes/{{$orden->archivo_cotizacion}}&embedded=true"
     width="100%" 
     height="1000" 
     style="border: none;"

56f7f7ee7777bceb8d3e15421092780c145667cf
Carlos Cuamatzin <kuamatzin@gmail.com>
2016-09-05 18:55:13 -0500

    CORS 2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Services\ReportGenerator;
+use Illuminate\Support\Facades\Mail;
 
 Route::get('/', 'HomeController@index');
 
@@ -10,6 +11,15 @@ Route::get('/reporte/{orden}', 'OrdenController@reporte');
 
 Route::get('/reporte/descarga/{orden}', 'OrdenController@descargaReporte');
 
-Route::get('/sendEmail/{nombre}/{email}', ['middleware' => 'cors', 'OrdenController@sendEmail']);
+Route::get('/sendEmail/{nombre}/{email}', ['middleware' => 'cors', function($nombre, $email)
+{
+    $user['nombre'] = $nombre;
+    $user['email'] = $email;
+    Mail::send('emails.nuevaPregunta', ['user' => $user], function ($m) use ($user) {
+        $m->from('contacto@winu.mx', 'Winu');
+
+        $m->to($user['email'], $user['nombre'])->subject('Nueva pregunta en Winu!');
+    });
+}]);
 
 Route::auth();
